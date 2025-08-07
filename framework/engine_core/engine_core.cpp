961a48c4348b875d9b2425950977969b889a7270
Steve Hurley <microsoft@hurleyworks.com>
2025-08-07 08:34:54 -0400

    removed unused code

--- framework/engine_core/engine_core.cpp
+++ framework/engine_core/engine_core.cpp
@@ -7,7 +7,6 @@
 #include "excludeFromBuild/GPUContext.cpp"
 #include "excludeFromBuild/RenderContext.cpp"
 
-
 // tools
 #include "excludeFromBuild/tools/PTXManager.cpp"
 #include "excludeFromBuild/tools/GPUManager.cpp"
@@ -18,21 +17,22 @@
 #include "excludeFromBuild/common/common_host.cpp"
 #include "excludeFromBuild/common/dds_loader.cpp"
 
-
 // nvcc
 #include "excludeFromBuild/nvcc/CudaCompiler.cpp"
 
-// handlers
+// commmon handlers
 #include "excludeFromBuild/handlers/SkyDomeHandler.cpp"
-#include "excludeFromBuild/handlers/DenoiserHandler.cpp"
+#include "excludeFromBuild/handlers/TextureHandler.cpp"
+#include "excludeFromBuild/handlers/AreaLightHandler.cpp"
+
+// milo handlers
 #include "excludeFromBuild/handlers/MiloSceneHandler.cpp"
 #include "excludeFromBuild/handlers/MiloMaterialHandler.cpp"
 #include "excludeFromBuild/handlers/MiloModelHandler.cpp"
 #include "excludeFromBuild/handlers/MiloRenderHandler.cpp"
-#include "excludeFromBuild/handlers/TextureHandler.cpp"
 #include "excludeFromBuild/handlers/MiloDenoiserHandler.cpp"
-#include "excludeFromBuild/handlers/AreaLightHandler.cpp"
-// shocker
+
+// shocker handlers
 #include "excludeFromBuild/handlers/ShockerModelHandler.cpp"
 #include "excludeFromBuild/handlers/ShockerMaterialHandler.cpp"
 #include "excludeFromBuild/handlers/ShockerSceneHandler.cpp"
@@ -42,13 +42,6 @@
 #include "excludeFromBuild/engines/RenderEngineManager.cpp"
 #include "excludeFromBuild/engines/MiloEngine.cpp"
 
-
-
 // models
 #include "excludeFromBuild/model/MiloModel.cpp"
 #include "excludeFromBuild/model/ShockerModel.cpp"
-
-
-
-
-
--- framework/engine_core/excludeFromBuild/handlers/Handlers.h
+++ framework/engine_core/excludeFromBuild/handlers/Handlers.h
@@ -7,10 +7,7 @@
 
 #include "../../engine_core.h"
 #include "SkyDomeHandler.h"
-#include "DenoiserHandler.h"
-//#include "RenderHandler.h"
 #include "TextureHandler.h"
-
 
 // #include "MaterialHandler.h"  // Now engine-specific, not shared
 // #include "ModelHandler.h"  // Not used in engine-only mode
@@ -31,75 +28,30 @@ struct Handlers
         // Initialize SkyDomeHandler
         skyDomeHandler = SkyDomeHandler::create (renderContext);
 
-        // Initialize DenoiserHandler (but don't set it up yet - that requires dimensions)
-        denoiserHandler = DenoiserHandler::create (renderContext);
-
-        // Initialize RenderHandler (but don't set it up yet - that requires dimensions)
-      // renderHandler = RenderHandler::create (renderContext);
-       
-        
         // Initialize TextureHandler
         textureHandler = TextureHandler::create (renderContext);
-        
-        // MaterialHandler is now engine-specific, not shared
-        // Each engine creates its own material handler
-        
-        // Initialize ModelHandler (after material handler)
-        // modelHandler = ModelHandler::create (renderContext);  // Not used in engine-only mode
     }
 
     // Cleanup all handlers
     void cleanup()
     {
-        // Cleanup RenderHandler
-       // if (renderHandler)
-       // {
-       //     renderHandler->finalize();
-       //     renderHandler.reset();
-       // }
-
-        // Cleanup DenoiserHandler
-        if (denoiserHandler)
-        {
-            denoiserHandler->finalize();
-            denoiserHandler.reset();
-        }
-
         // Cleanup SkyDomeHandler
         if (skyDomeHandler)
         {
             skyDomeHandler->finalize();
             skyDomeHandler.reset();
         }
-         
+
         // Cleanup TextureHandler
         if (textureHandler)
         {
             // TextureHandler's destructor handles cleanup
             textureHandler.reset();
         }
-        
-        // MaterialHandler is now engine-specific, cleaned up by each engine
-        
-        // Cleanup ModelHandler
-        // if (modelHandler)
-        // {
-        //     // ModelHandler's destructor handles cleanup
-        //     modelHandler.reset();
-        // }  // Not used in engine-only mode
     }
 
     // Handler members
     SkyDomeHandlerPtr skyDomeHandler;
-    DenoiserHandlerPtr denoiserHandler;
-   // RenderHandlerPtr renderHandler;
-    TextureHandlerPtr textureHandler;
-    // MaterialHandlerPtr materialHandler;  // Now engine-specific, not shared
-    // ModelHandlerPtr modelHandler;  // Not used in engine-only mode
 
-    // Additional handlers will be added here as we implement them:
-    // - MaterialHandler
-    // - LightHandler
-    // - CameraHandler
-    // - etc.
+    TextureHandlerPtr textureHandler;
 };
