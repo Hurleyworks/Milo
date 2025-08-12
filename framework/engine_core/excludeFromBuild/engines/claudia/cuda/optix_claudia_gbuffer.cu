56e7ac93d6ca05e72af7780f94dc4b5712bc7f29
Steve Hurley <microsoft@hurleyworks.com>
2025-08-12 08:09:34 -0400

    starting gbuffer support

---
+++ framework/engine_core/excludeFromBuild/engines/claudia/cuda/optix_claudia_gbuffer.cu
@@ -0,0 +1,21 @@
+
+
+#include "principledDisney_claudia.h"
+#include "../claudia_shared.h"
+
+using namespace claudia_shared;
+
+// Global declaration of pipeline launch parameters - must match the name in pipeline configuration
+RT_PIPELINE_LAUNCH_PARAMETERS claudia_shared::PipelineLaunchParameters claudia_plp;
+
+CUDA_DEVICE_KERNEL void RT_RG_NAME(setupGBuffers)() {
+    
+}
+
+CUDA_DEVICE_KERNEL void RT_CH_NAME(setupGBuffers)() {
+  
+}
+
+CUDA_DEVICE_KERNEL void RT_MS_NAME(setupGBuffers)() {
+    
+}
